43c5be67cc0797f463424883d41f213c385b0005
CARLOS SOLANA MELERO <872815@unizar.es>
2025-02-08 13:37:34 +0100

    Añadido Swagger básico en el main pero un no se puede probar

--- echo-beat-backend/src/main.ts
+++ echo-beat-backend/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Configuración básica de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Echo Beat Backend')
+    .setDescription('API de streaming de música')
+    .setVersion('1.0')
+    .addBearerAuth() // si usas autenticación JWT
+    .build();
+
+  // Generar el documento a partir de la config
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Habilitar la ruta /api para visualizar la documentación
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
