7a1cb3da71a4c2b5c74ec9a4f446586f071cda42
CARLOS SOLANA MELERO <110463252+Csolanascript@users.noreply.github.com>
2025-02-27 12:38:01 +0100

    solucionado error prisma en generro

--- echo-beat-backend/src/genero/genero.module.ts
+++ echo-beat-backend/src/genero/genero.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GeneroService } from './genero.service';
 import { GeneroController } from './genero.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { PrismaService } from '../prisma/prisma.service';
+
 
 @Module({
+  imports: [PrismaModule],
+  controllers: [GeneroController],
   providers: [GeneroService],
-  controllers: [GeneroController]
+  exports: [GeneroService]
 })
 export class GeneroModule {}
