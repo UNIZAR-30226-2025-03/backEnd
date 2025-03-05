604e994a3bcc0b8bb0a83b9b3a392ad4d13120c4
CARLOS SOLANA MELERO <872815@unizar.es>
2025-03-05 20:36:01 +0100

    añadido https

--- echo-beat-backend/src/main.ts
+++ echo-beat-backend/src/main.ts
@@ -1,11 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  // Habilita CORS para todas las peticiones
+  const port = process.env.PORT || 3000;
+  let app;
+
+  // Si en el .env la variable HTTPS es "YES", se configura HTTPS
+  if (process.env.HTTPS === 'YES') {
+    // Lee el dominio desde la variable DUCKDNS_DOMAIN 
+    const domain = process.env.DUCKDNS_DOMAIN;
+    // Configura las opciones HTTPS con los certificados de Let's Encrypt para ese dominio
+    const httpsOptions = {
+      key: fs.readFileSync(`/etc/letsencrypt/live/${domain}/privkey.pem`),
+      cert: fs.readFileSync(`/etc/letsencrypt/live/${domain}/fullchain.pem`),
+    };
+
+    app = await NestFactory.create(AppModule, { httpsOptions });
+    // HTTPS normalmente usa el puerto 443
+    await app.listen(443, '0.0.0.0');
+    console.log(`Servidor corriendo en https://${domain}`);
+  } else {
+    // Si HTTPS no está habilitado, se inicia en HTTP
+    app = await NestFactory.create(AppModule);
+    await app.listen(port, '0.0.0.0');
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+  }
+
+  // Habilitar CORS para todas las peticiones
   app.enableCors();
+
   // Configuración básica de Swagger
   const config = new DocumentBuilder()
     .setTitle('Echo Beat Backend')
@@ -14,17 +39,7 @@ async function bootstrap() {
     .addBearerAuth() // si usas autenticación JWT
     .build();
 
-  // Generar el documento a partir de la config
   const document = SwaggerModule.createDocument(app, config);
-
-  // Habilitar la ruta /api para visualizar la documentación
   SwaggerModule.setup('api', app, document);
-
-  // Usa una variable de entorno para el puerto o el valor por defecto 3000
-  const port = process.env.PORT || 3000;
-  
-  // Escucha en todas las interfaces de red ('0.0.0.0') para exponer el puerto
-  await app.listen(port, '0.0.0.0');
-  console.log(`Servidor corriendo en http://localhost:${port}`);
 }
 bootstrap();
