2ba46357d07c899f9818fbccfe7285e4ae7ea03f
Jordan888-8 <jordanmarcomorales@gmail.com>
2025-03-12 13:10:22 +0100

    modulo amistades

--- echo-beat-backend/src/amistades/amistades.module.ts
+++ echo-beat-backend/src/amistades/amistades.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AmistadesController } from './amistades.controller';
 import { AmistadesService } from './amistades.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [AmistadesController],
-  providers: [AmistadesService]
+  providers: [AmistadesService, PrismaService]
 })
 export class AmistadesModule {}
