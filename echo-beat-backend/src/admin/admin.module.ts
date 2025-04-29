03e67ceb84885ce3423f1da1d9fa7b821bacae47
CARLOS SOLANA MELERO <110463252+Csolanascript@users.noreply.github.com>
2025-04-29 14:40:33 +0200

    prueba sin comentar

--- echo-beat-backend/src/admin/admin.module.ts
+++ echo-beat-backend/src/admin/admin.module.ts
@@ -3,11 +3,11 @@ import { PrismaService } from '../prisma/prisma.service';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { AuthModule } from 'src/auth/auth.module'; // Ajusta la ruta según la estructura de tu proyecto
-//import { AdminAuthGuard } from './admin-auth.guard'; // Asegúrate de que la ruta sea correcta
+import { AdminAuthGuard } from './admin-auth.guard'; // Asegúrate de que la ruta sea correcta
 
 @Module({
   imports: [AuthModule], // Aquí se importa el módulo que provee JwtModule (y JwtService)
   controllers: [AdminController],
-  providers: [AdminService, PrismaService],
+  providers: [AdminService, PrismaService, AdminAuthGuard],
 })
 export class AdminModule {}
