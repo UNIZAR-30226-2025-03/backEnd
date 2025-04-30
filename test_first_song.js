32c7881517c83d37d58fcf07709a4a2b423cc8e8
CARLOS SOLANA MELERO <110463252+Csolanascript@users.noreply.github.com>
2025-04-30 17:20:23 +0200

    prueba con varios workers en pm2

---
+++ test_first_song.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  vus: 500,           // Usuarios virtuales concurrentes
+  duration: '60s',   // Duración total del test
+};
+
+const email = 'aaaaaa%40gmail.com';  // Email válido de prueba
+
+export default function () {
+  const url = `https://echobeatapi.duckdns.org/users/first-song?Email=aaaaaa%40gmail.com`;
+  //const url = `https://nogler.ddns.net/allusers`;
+  const res = http.get(url);
+
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+    'body is not empty': (r) => r.body.length > 0,
+  });
+  console.log(`Status: ${res.status} - Body: ${res.body}`);
+
+
+  sleep(0,5);  // Pausa de 1 segundo entre peticiones por usuario virtual
+}
